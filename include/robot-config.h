bc17008da8e64aae5a12de9c84be34995f1ebe37
Ryan McGee <ramtech51@comcast.net>
2023-10-01 12:37:13 -0400

    Cleaned up opcontrol/robot-config

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,43 +1,21 @@
 #pragma once
 #include "vex.h"
-#include "core.h"
 
-using namespace vex;
-
-extern brain Brain;
-extern controller con;
+extern vex::brain brain;
+extern vex::controller con;
 
 // ================ INPUTS ================
 // Digital sensors
-extern CustomEncoder left_odom_wheel;
-extern CustomEncoder right_odom_wheel;
 
 // Analog sensors
-extern inertial imu;
+
 
 // ================ OUTPUTS ================
 // Motors
-extern motor left_front;
-extern motor left_back;
-extern motor right_front;
-extern motor right_back;
-extern motor_group left_drive;
-extern motor_group right_drive;
+
 
 // ================ SUBSYSTEMS ================
-extern PID::pid_config_t drive_pid_cfg;
-extern PID::pid_config_t turn_pid_cfg;
 
-extern FeedForward::ff_config_t drive_ff_cfg;
-extern FeedForward::ff_config_t turn_ff_cfg;
-
-extern MotionController::m_profile_cfg_t drive_profile_cfg;
-extern MotionController::m_profile_cfg_t turn_profile_cfg;
-
-extern robot_specs_t robot_cfg;
-
-extern OdometryTank odometry;
-extern TankDrive drive_system;
 
 // ================ UTILS ================
 
--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -1,5 +1,4 @@
 #include "competition/opcontrol.h"
-#include "robot-config.h"
 #include "vex.h"
 
 /**
@@ -8,46 +7,12 @@
 void opcontrol()
 {
     // ================ INIT ================
-    imu.calibrate();
-    while(imu.isCalibrating());
-    
-    static std::vector<point_t> path1 = {
-        {0, 0},
-        {0, 24},
-        {24, 24},
-        {24, 48}
-    };
 
-    static std::vector<point_t> path2 = {
-        {24, 48},
-        {24, 24},
-        {0, 24},
-        {0, 0},
-    };
-
-    odometry.set_position();
-
-    static std::atomic<bool> auto_driving(false);
-    con.ButtonA.pressed([](){
-        auto_driving = true;
-        CommandController cmd;
-        cmd.add(new PurePursuitCommand(drive_system, *robot_cfg.drive_feedback, path1, fwd, 12, .4));
-        cmd.add(new PurePursuitCommand(drive_system, *robot_cfg.drive_feedback, path2, directionType::rev, 12, .4));
-        cmd.run();
-        auto_driving = false;
-    });
 
     // ================ PERIODIC ================
     while(true)
     {
-        if(!auto_driving)
-            drive_system.drive_arcade(con.Axis3.position()/100.0, con.Axis1.position()/100.0);
 
-        if(con.ButtonB.pressing())
-            odometry.set_position();
-
-        pose_t pos = odometry.get_position();
-        printf("x:%f, y:%f, rot:%f\n", pos.x, pos.y, pos.rot);
         vexDelay(10);
     }
 }
