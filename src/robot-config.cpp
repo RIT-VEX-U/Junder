1392fa29b378177c92ddef16e98dcf46f7254bf2
cowsed <richie.sommers@outlook.com>
2023-10-10 22:35:48 -0400

    odometry map

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,27 +7,38 @@ vex::controller con;
 inertial imu(PORT10);
 
 // 1 is frontmost, 4 is rearmost
-vex::motor left1(vex::PORT1, vex::gearSetting::ratio6_1, true);
-vex::motor left2(vex::PORT2, vex::gearSetting::ratio6_1, true);
-vex::motor left3(vex::PORT3, vex::gearSetting::ratio6_1, true);
-vex::motor left4(vex::PORT4, vex::gearSetting::ratio6_1, true);
+vex::motor left1(vex::PORT11, vex::gearSetting::ratio6_1, true);
+vex::motor left2(vex::PORT22, vex::gearSetting::ratio6_1, true);
+vex::motor left3(vex::PORT13, vex::gearSetting::ratio6_1, true);
+vex::motor left4(vex::PORT14, vex::gearSetting::ratio6_1, true);
 
-vex::motor right1(vex::PORT1, vex::gearSetting::ratio6_1, false);
-vex::motor right2(vex::PORT2, vex::gearSetting::ratio6_1, false);
-vex::motor right3(vex::PORT3, vex::gearSetting::ratio6_1, false);
-vex::motor right4(vex::PORT4, vex::gearSetting::ratio6_1, false);
+vex::motor right1(vex::PORT15, vex::gearSetting::ratio6_1, false);
+vex::motor right2(vex::PORT16, vex::gearSetting::ratio6_1, false);
+vex::motor right3(vex::PORT17, vex::gearSetting::ratio6_1, false);
+vex::motor right4(vex::PORT18, vex::gearSetting::ratio6_1, false);
 
 vex::motor_group left_motors(left1, left2, left3, left4);
 vex::motor_group right_motors(right1, right2, right3, right4);
 
-vex::motor cata1(vex::PORT9, vex::gearSetting::ratio36_1, true);
-vex::motor cata2(vex::PORT10, vex::gearSetting::ratio36_1, true);
+vex::motor cata_r(vex::PORT4, vex::gearSetting::ratio36_1, false);
+vex::motor cata_l(vex::PORT5, vex::gearSetting::ratio36_1, true);
 
-vex::motor_group cata_motors(cata1, cata2);
+vex::motor_group cata_motors(cata_l, cata_r);
 
+std::map<std::string, vex::motor &> motor_names = {
+    {"left 1", left1},
+    {"left 2", left2},
+    {"left 3", left3},
+    {"left 4", left4},
 
-std::map<std::string, vex::motor&> motor_names = {};
+    {"right 1", right1},
+    {"right 2", right2},
+    {"right 3", right3},
+    {"right 4", right4},
 
+    {"cata L", cata_l},
+    {"cata R", cata_r},
+};
 
 PID::pid_config_t drive_pid_cfg =
     {
@@ -59,7 +70,6 @@ robot_specs_t robot_cfg = {
 
 OdometryTank odom{left_motors, right_motors, robot_cfg};
 TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
-
 
 #else
 
@@ -122,12 +132,14 @@ TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
 
 #endif
 
-std::vector<screen::Page*> pages = {new screen::MotorPage(motor_names)};
+std::vector<screen::Page *> pages;
 /**
  * Main robot initialization on startup. Runs before opcontrol and autonomous are started.
  */
 void robot_init()
 {
+    pages = {new screen::StatsPage(motor_names), new screen::OdometryPage(odom, 12, 12)};
+    odom.set_position({36, 36, 45});
     imu.calibrate();
-    screen::start_screen(Brain.Screen, pages);
+    screen::start_screen(Brain.Screen, pages, 1);
 }
