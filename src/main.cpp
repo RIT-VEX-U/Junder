07a005cd34d8d2b851cc21cdd844c76c1f5452e3
cowsed <richie.sommers@outlook.com>
2023-10-15 16:20:40 -0400

    no more test code

--- src/main.cpp
+++ src/main.cpp
@@ -25,21 +25,6 @@ vex::competition comp;
 
 int main()
 {
-    printf("ma \t ema \t sensor\n");
-    ExponentialMovingAverage ema{4};
-    MovingAverage ma{4};
-    for (int j = 0; j < 20; j++)
-    {
-        int i = j;
-        if (j > 10)
-        {
-            i = 10;
-        }
-        ema.add_entry((double)i);
-        ma.add_entry((double)i);
-        printf("%f \t %f \t %f\n", ma.get_average(), ema.get_average(), (double)i);
-        fflush(stdout);
-    }
 
     comp.autonomous(autonomous);
     comp.drivercontrol(opcontrol);
