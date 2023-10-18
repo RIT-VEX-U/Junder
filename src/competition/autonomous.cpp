c3018e41d38f05a524456d41289d0b1c2d60be3f
Ethan <85726497+LilacEthan@users.noreply.github.com>
2023-10-17 20:44:15 -0400

    Emptied out Autonomous
    Got rid of the testing commands in Auto function

--- src/competition/autonomous.cpp
+++ src/competition/autonomous.cpp
@@ -6,16 +6,8 @@
  * Main entrypoint for the autonomous period
  */
 
-vex::motor motor3{PORT3};
 
 void autonomous()
 {
-    CommandController basic {
-        new BasicSpinCommand(motor3, vex::directionType::fwd, BasicSpinCommand::type::percent, 100),
-        new DelayCommand(1000),
-        new BasicStopCommand(motor3, coast),
 
-    };
-
-    basic.run();
 }
