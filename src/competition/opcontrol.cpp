1695b09fe939e8c317767818f8053d6c8b4899b3
cowsed <richie.sommers@outlook.com>
2023-12-01 17:45:23 -0500

    motion controller screen

--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -1,7 +1,7 @@
 #include "competition/opcontrol.h"
-#include "vex.h"
-#include "robot-config.h"
 #include "automation.h"
+#include "robot-config.h"
+#include "vex.h"
 #include <atomic>
 
 // #define Tank
@@ -9,77 +9,78 @@
 /**
  * Main entrypoint for the driver control period
  */
-void opcontrol()
-{
-    // vexDelay(1000);
+void opcontrol() {
+  // vexDelay(1000);
 
-    while (imu.isCalibrating()) // || gps_sensor.isCalibrating())
-    {
-        vexDelay(20);
-    }
+  while (imu.isCalibrating()) // || gps_sensor.isCalibrating())
+  {
+    vexDelay(20);
+  }
 
-    static bool enable_matchload = false;
+  con.ButtonA.pressed([] {
+    CommandController cc{
+        drive_sys.DriveForwardCmd(5.0, vex::fwd),
+    };
+    cc.run();
+  });
 
-    // Controls:
-    // Cata: Hold L1 (Not on rising edge)
-    // -- Don't shoot until there's a ball
-    // -- Preload
-    // Intake:
-    // -- R1 IN
-    // -- R2 OUT
-    // -- B - 2 intake pistons
+  static bool enable_matchload = false;
 
-    // SUBJECT TO CHANGE!
-    // Wings: DOWN
+  // Controls:
+  // Cata: Hold L1 (Not on rising edge)
+  // -- Don't shoot until there's a ball
+  // -- Preload
+  // Intake:
+  // -- R1 IN
+  // -- R2 OUT
+  // -- B - 2 intake pistons
+
+  // SUBJECT TO CHANGE!
+  // Wings: DOWN
 #ifdef COMP_BOT
-    con.ButtonL1.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::StartFiring); });
-    con.ButtonL1.released([]()
-                          { cata_sys.send_command(CataSys::Command::StopFiring); });
-    con.ButtonR1.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeIn); });
-    con.ButtonR2.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeOut); });
-    con.ButtonL2.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeHold); });
+  con.ButtonL1.pressed(
+      []() { cata_sys.send_command(CataSys::Command::StartFiring); });
+  con.ButtonL1.released(
+      []() { cata_sys.send_command(CataSys::Command::StopFiring); });
+  con.ButtonR1.pressed(
+      []() { cata_sys.send_command(CataSys::Command::IntakeIn); });
+  con.ButtonR2.pressed(
+      []() { cata_sys.send_command(CataSys::Command::IntakeOut); });
+  con.ButtonL2.pressed(
+      []() { cata_sys.send_command(CataSys::Command::IntakeHold); });
 
-    con.ButtonRight.pressed([]()
-                            { screen::next_page(); });
-    con.ButtonLeft.pressed([]()
-                            { screen::prev_page(); });
+  con.ButtonRight.pressed([]() { screen::next_page(); });
+  con.ButtonLeft.pressed([]() { screen::prev_page(); });
 
-    con.ButtonDown.pressed([]()
-                           { left_wing.set(!left_wing.value()); });
-    con.ButtonB.pressed([]()
-                        { right_wing.set(!right_wing.value()); });
+  con.ButtonDown.pressed([]() { left_wing.set(!left_wing.value()); });
+  con.ButtonB.pressed([]() { right_wing.set(!right_wing.value()); });
 
 #endif
-    // ================ INIT ================
-    while (true)
-    {
-        if (!con.ButtonR1.pressing() && !con.ButtonR2.pressing() && !con.ButtonL2.pressing())
-        {
-            cata_sys.send_command(CataSys::Command::StopIntake);
-        }
+  // ================ INIT ================
+  while (true) {
+    if (!con.ButtonR1.pressing() && !con.ButtonR2.pressing() &&
+        !con.ButtonL2.pressing()) {
+      cata_sys.send_command(CataSys::Command::StopIntake);
+    }
 #ifdef Tank
-        double l = con.Axis3.position() / 100.0;
-        double r = con.Axis2.position() / 100.0;
-        drive_sys.drive_tank(l, r, 1, TankDrive::BrakeType::Smart);
+    double l = con.Axis3.position() / 100.0;
+    double r = con.Axis2.position() / 100.0;
+    drive_sys.drive_tank(l, r, 1, TankDrive::BrakeType::Smart);
 
 #else
 
-        double f = con.Axis3.position() / 100.0;
-        double s = con.Axis1.position() / 100.0;
-        drive_sys.drive_arcade(f, s, 1, TankDrive::BrakeType::Smart);
+    double f = con.Axis3.position() / 100.0;
+    double s = con.Axis1.position() / 100.0;
+    drive_sys.drive_arcade(f, s, 1, TankDrive::BrakeType::Smart);
 #endif
 
-        // matchload_1(enable_matchload); // Toggle
-        matchload_1([](){ return con.ButtonA.pressing();}); // Hold
-        // Controls
-        // Intake
+    // matchload_1(enable_matchload); // Toggle
+    matchload_1([]() { return con.ButtonA.pressing(); }); // Hold
+    // Controls
+    // Intake
 
-        vexDelay(10);
-    }
+    vexDelay(10);
+  }
 
-    // ================ PERIODIC ================
+  // ================ PERIODIC ================
 }
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -18,23 +18,25 @@ constexpr gearSetting drive_gears = gearSetting::ratio18_1;
 motor left_front_front(PORT17, drive_gears, true); // Final Port
 motor left_front_back(PORT18, drive_gears, false); // Final Port
 motor left_back_front(PORT19, drive_gears, false); // Final Port
-motor left_back_back(PORT20, drive_gears, true); // Final Port
+motor left_back_back(PORT20, drive_gears, true);   // Final Port
 
 motor right_front_front(PORT14, drive_gears, false); // Final Port
-motor right_front_back(PORT13, drive_gears, true); // Final Port
-motor right_back_front(PORT12, drive_gears, true); // Final Port
-motor right_back_back(PORT11, drive_gears, false); // Final Port
+motor right_front_back(PORT13, drive_gears, true);   // Final Port
+motor right_back_front(PORT12, drive_gears, true);   // Final Port
+motor right_back_back(PORT11, drive_gears, false);   // Final Port
 
-motor_group left_motors(left_front_front, left_front_back, left_back_front, left_back_back);
-motor_group right_motors(right_front_front, right_front_back, right_back_front, right_back_back);
+motor_group left_motors(left_front_front, left_front_back, left_back_front,
+                        left_back_back);
+motor_group right_motors(right_front_front, right_front_back, right_back_front,
+                         right_back_back);
 
-motor cata_r(PORT2, gearSetting::ratio36_1, true); // Final Port
+motor cata_r(PORT2, gearSetting::ratio36_1, true);   // Final Port
 motor cata_l(PORT10, gearSetting::ratio36_1, false); // Final Port
 
 motor_group cata_motors(cata_l, cata_r);
 
 motor intake_combine(PORT1, gearSetting::ratio18_1, false); // Final Port
-motor intake_roller(PORT9, gearSetting::ratio18_1, false); // Final Port
+motor intake_roller(PORT9, gearSetting::ratio18_1, false);  // Final Port
 
 motor_group intake_motors = {intake_combine, intake_roller};
 
@@ -58,49 +60,37 @@ std::map<std::string, motor &> motor_names = {
 };
 
 // ================ SUBSYSTEMS ================
-PID::pid_config_t drive_pid_cfg =
-    {
-        .p = .2,
-        .i = 0.0,
-        .d = .021,
-        .deadband = 0.5,
-        .on_target_time = .1};
+PID::pid_config_t drive_pid_cfg = {
+    .p = .2, .i = 0.0, .d = .021, .deadband = 0.5, .on_target_time = .1};
 
-PID::pid_config_t turn_pid_cfg =
-    {
-        .p = 0.05,
-        .i = 0.00,
-        .d = 0.0020,
-        .deadband = 3,
-        .on_target_time = 0.1};
+PID::pid_config_t turn_pid_cfg = {
+    .p = 0.05, .i = 0.00, .d = 0.0020, .deadband = 3, .on_target_time = 0.1};
 
-
-MotionController::m_profile_cfg_t drive_mc_cfg{
-    .max_v= 72.50,
-    .accel = 190.225,
-    .pid_cfg = drive_pid_cfg,
-    .ff_cfg = FeedForward::ff_config_t{
-        .kA = 0.0029,
-        .kV = 0.0131,
-        .kS = 0.05,
-        .kG = 0,
-    }
-};
+MotionController::m_profile_cfg_t drive_mc_cfg{.max_v = 72.50,
+                                               .accel = 190.225,
+                                               .pid_cfg = drive_pid_cfg,
+                                               .ff_cfg =
+                                                   FeedForward::ff_config_t{
+                                                       .kA = 0.0029,
+                                                       .kV = 0.0131,
+                                                       .kS = 0.05,
+                                                       .kG = 0,
+                                                   }};
 MotionController drive_mc{drive_mc_cfg};
 
-robot_specs_t robot_cfg = {
-    .robot_radius = 12,            // inches
-    .odom_wheel_diam = 3.15,          // inches
-    .odom_gear_ratio = .5,        
-    .dist_between_wheels = 10.6,    // inches
-    .drive_correction_cutoff = 4, // inches
-    .drive_feedback = new PID(drive_pid_cfg),//&drive_mc,
-    .turn_feedback = new PID(turn_pid_cfg),
-    .correction_pid = (PID::pid_config_t){
-        .p = .03,
-    }};
+robot_specs_t robot_cfg = {.robot_radius = 12,      // inches
+                           .odom_wheel_diam = 3.15, // inches
+                           .odom_gear_ratio = .5,
+                           .dist_between_wheels = 10.6,  // inches
+                           .drive_correction_cutoff = 4, // inches
+                           .drive_feedback =
+                               new PID(drive_pid_cfg), //&drive_mc,
+                           .turn_feedback = new PID(turn_pid_cfg),
+                           .correction_pid = (PID::pid_config_t){
+                               .p = .03,
+                           }};
 
-OdometryTank odom{left_motors, right_motors, robot_cfg,&imu};
+OdometryTank odom{left_motors, right_motors, robot_cfg, &imu};
 TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
 
 vex::distance intake_watcher(vex::PORT3);
@@ -112,7 +102,8 @@ vex::pot cata_pot(Brain.ThreeWirePort.E);
 vex::digital_out left_wing(Brain.ThreeWirePort.G);
 vex::digital_out right_wing(Brain.ThreeWirePort.H);
 
-CataSys cata_sys(intake_watcher, cata_pot, cata_watcher, cata_motors, intake_roller, intake_combine);
+CataSys cata_sys(intake_watcher, cata_pot, cata_watcher, cata_motors,
+                 intake_roller, intake_combine);
 
 #else
 
@@ -152,33 +143,22 @@ CustomEncoder right_enc = CustomEncoder{Brain.ThreeWirePort.A, 90};
 CustomEncoder left_enc = CustomEncoder{Brain.ThreeWirePort.C, 90};
 
 // ======== SUBSYSTEMS ========
-PID::pid_config_t drive_pid_cfg =
-    {
-        .p = .24,
-        .i = 0.0,
-        .d = .01,
-        .deadband = 0.1,
-        .on_target_time = 0};
+PID::pid_config_t drive_pid_cfg = {
+    .p = .24, .i = 0.0, .d = .01, .deadband = 0.1, .on_target_time = 0};
 
-PID::pid_config_t turn_pid_cfg =
-    {
-        .p = 0.014,
-        .i = 0.01,
-        .d = 0.0025,
-        .deadband = 1,
-        .on_target_time = 0.1};
+PID::pid_config_t turn_pid_cfg = {
+    .p = 0.014, .i = 0.01, .d = 0.0025, .deadband = 1, .on_target_time = 0.1};
 
-robot_specs_t robot_cfg = {
-    .robot_radius = 12,            // inches
-    .odom_wheel_diam = 2.84,       // inches
-    .odom_gear_ratio = 1.03,       // inches
-    .dist_between_wheels = 9.18,   // inches
-    .drive_correction_cutoff = 12, // inches
-    .drive_feedback = new PID(drive_pid_cfg),
-    .turn_feedback = new PID(turn_pid_cfg),
-    .correction_pid = (PID::pid_config_t){
-        .p = .03,
-    }};
+robot_specs_t robot_cfg = {.robot_radius = 12,            // inches
+                           .odom_wheel_diam = 2.84,       // inches
+                           .odom_gear_ratio = 1.03,       // inches
+                           .dist_between_wheels = 9.18,   // inches
+                           .drive_correction_cutoff = 12, // inches
+                           .drive_feedback = new PID(drive_pid_cfg),
+                           .turn_feedback = new PID(turn_pid_cfg),
+                           .correction_pid = (PID::pid_config_t){
+                               .p = .03,
+                           }};
 
 OdometryTank odom{left_enc, right_enc, robot_cfg, &imu};
 TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
@@ -189,19 +169,20 @@ TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
 std::vector<screen::Page *> pages;
 
 /**
- * Main robot initialization on startup. Runs before opcontrol and autonomous are started.
+ * Main robot initialization on startup. Runs before opcontrol and autonomous
+ * are started.
  */
-void robot_init()
-{
+void robot_init() {
 
-    pages = {
-        new AutoChooser({"Auto 1", "Auto 2", "Auto 3", "Auto 4"}),
-        new screen::StatsPage(motor_names),
-        new screen::OdometryPage(odom, 12, 12, true),
-        cata_sys.Page(),
-    };
+  pages = {
+      new AutoChooser({"Auto 1", "Auto 2", "Auto 3", "Auto 4"}),
+      new screen::StatsPage(motor_names),
+      new screen::OdometryPage(odom, 12, 12, true),
+      cata_sys.Page(),
+      drive_mc.Page(),
+  };
 
-    screen::start_screen(Brain.Screen, pages, 2);
-    // imu.calibrate();
-    // gps_sensor.calibrate();
+  screen::start_screen(Brain.Screen, pages, 4);
+  // imu.calibrate();
+  // gps_sensor.calibrate();
 }
