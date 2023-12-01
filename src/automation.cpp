2a5b1e1ddc24e9e6cfe03faa25d1a9c7af7df56d
Ryan McGee <ramtech51@comcast.net>
2023-12-01 15:30:00 -0500

    Added to matchloading

--- src/automation.cpp
+++ src/automation.cpp
@@ -1,17 +1,24 @@
 #include "automation.h"
 #include "robot-config.h"
 
+#define FWD vex::directionType::fwd
+#define REV vex::directionType::rev
+
 // ================ Autonomous Abstractions ================
 
 
 // ================ Driver Assist Automations ================
 
-void matchload_1(vex::controller::button &button)
+void matchload_1(bool &enable)
 {
-    // Determine which side with imu
-    bool shootLeft = true;
+    matchload_1([enable](){return enable;});
+}
 
-
+void matchload_1(std::function<bool()> enable)
+{
+    if(!enable())
+        return;
+        
     FunctionCommand *waitForIntake = new FunctionCommand([&](){
         return intake_watcher.objectDistance(distanceUnits::mm) < 150;
     });
@@ -19,24 +26,17 @@ void matchload_1(vex::controller::button &button)
     CommandController cmd{
         cata_sys.IntakeFully(),
         waitForIntake, 
-
+        drive_sys.DriveForwardCmd(6, FWD, .5),
+        drive_sys.TurnDegreesCmd(-10, .5),
+        cata_sys.Fire(), 
+        new DelayCommand(300),
+        drive_sys.DriveForwardCmd(6, REV, .5),
+        drive_sys.TurnDegreesCmd(10, .5),
+        cata_sys.IntakeFully()
     };
     
     // Cancel the operation if the button is ever released
-    cmd.add_cancel_func([&](){return !button.pressing();});
+    cmd.add_cancel_func([&](){return !enable(); });
     
     cmd.run();
-
-    // CommandController cmd{
-    // cata_sys.IntakeFully()->run();
-    // while(!waitForIntake.run() && button) {vexDelay(10);}
-    // while(!drive_sys.drive_forward(6, vex::directionType::rev, .5) && button ){ vexDelay(10); }
-    // while(!drive_sys.turn_degrees(-10, .5) && button) { vexDelay(10); }
-    // cata_sys.Fire()->run();
-    // vexDelay(300);
-    // while(!drive_sys.turn_degrees(10, .5) && button) {vexDelay(10);}
-    // while(!drive_sys.drive_forward(6, vex::directionType::fwd, .5) && button) {vexDelay(10);}
-        
-    // };
-
 }
--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -2,6 +2,7 @@
 #include "vex.h"
 #include "robot-config.h"
 #include "automation.h"
+#include <atomic>
 
 // #define Tank
 
@@ -17,49 +18,28 @@ void opcontrol()
         vexDelay(20);
     }
 
-    // odom.set_position({.x = 16, .y = 144-16, .rot = 135});
-    // CommandController cc{
-        // 
-        // drive_sys.DriveForwardCmd(36, vex::directionType::rev, 0.9),
-        // drive_sys.TurnToHeadingCmd(-90),
-        // drive_sys.DriveToPointCmd({.x = 27, .y = 18}, vex::fwd)
-    // };
-    // cc.add_cancel_func([]()
-                    //    {  return con.ButtonA.pressing(); });
-    // cc.run();
-    // return;
+    static bool enable_matchload = false;
 
-// while (imu.isCalibrating()) // || gps_sensor.isCalibrating())
-// {
-//     vexDelay(20);
-// }
+    // Controls:
+    // Cata: Hold L1 (Not on rising edge)
+    // -- Don't shoot until there's a ball
+    // -- Preload
+    // Intake:
+    // -- R1 IN
+    // -- R2 OUT
+    // -- B - 2 intake pistons
 
-// Controls:
-// Cata: Hold L1 (Not on rising edge)
-// -- Don't shoot until there's a ball
-// -- Preload
-// Intake:
-// -- R1 IN
-// -- R2 OUT
-// -- B - 2 intake pistons
-
-// SUBJECT TO CHANGE!
-// Wings: DOWN
+    // SUBJECT TO CHANGE!
+    // Wings: DOWN
 #ifdef COMP_BOT
-    con.ButtonL1.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::StartFiring); });
-    con.ButtonL1.released([]()
-                          { cata_sys.send_command(CataSys::Command::StopFiring); });
-    con.ButtonR1.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeIn); });
-    con.ButtonR2.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeOut); });
-    con.ButtonL2.pressed([]()
-                         { cata_sys.send_command(CataSys::Command::IntakeHold); });
-    con.ButtonDown.pressed([]()
-                           { left_wing.set(!left_wing.value()); });
-    con.ButtonB.pressed([]()
-                        { right_wing.set(!right_wing.value()); });
+    con.ButtonL1.pressed(   [](){ cata_sys.send_command(CataSys::Command::StartFiring); });
+    con.ButtonL1.released(  [](){ cata_sys.send_command(CataSys::Command::StopFiring); });
+    con.ButtonR1.pressed(   [](){ cata_sys.send_command(CataSys::Command::IntakeIn); });
+    con.ButtonR2.pressed(   [](){ cata_sys.send_command(CataSys::Command::IntakeOut); });
+    con.ButtonL2.pressed(   [](){ cata_sys.send_command(CataSys::Command::IntakeHold); });
+    con.ButtonDown.pressed( [](){ left_wing.set(!left_wing.value()); });
+    con.ButtonB.pressed(    [](){ right_wing.set(!right_wing.value()); });
+    con.ButtonA.pressed(    [](){ enable_matchload = !enable_matchload; });
 
 #endif
     // ================ INIT ================
@@ -81,7 +61,8 @@ void opcontrol()
         drive_sys.drive_arcade(f, s);
 #endif
 
-        // matchload_1(con.ButtonA.pressing());
+        // matchload_1(enable_matchload); // Toggle
+        matchload_1([](){ return con.ButtonA.pressing();}); // Hold
         // Controls
         // Intake
 
