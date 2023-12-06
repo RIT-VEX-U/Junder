f8010990ff7c307959824d37250fd963e63bddb5
cowsed <richie.sommers@outlook.com>
2023-12-05 20:13:34 -0500

    tuning parameters. who tunes em? idk

--- core/include/subsystems/odometry/odometry_gps.h
+++ core/include/subsystems/odometry/odometry_gps.h
@@ -1,35 +1,55 @@
 #pragma once
-#include <vector>
-#include <tuple>
-#include "../core/include/subsystems/odometry/odometry_tank.h"
 #include "../core/include/subsystems/odometry/odometry_3wheel.h"
+#include "../core/include/subsystems/odometry/odometry_tank.h"
+#include "../core/include/utils/math_util.h"
 #include "../core/include/utils/vector2d.h"
+#include <tuple>
+#include <vector>
 
-typedef struct
-{
-    double cutoff_radius;
+typedef struct {
+  double cutoff_radius;
+  /**
+   * normalized distance (0 at center 1 at corner) raised to this power.
+   * Higher values mean stay good for farther then switch rapidly at the end
+   * Lower values mean only prefer the gps when you're far from the walls
+   */
+  double distance_power;
+  /**
+   * Controls how much we want to discard old updates.
+   * If time factor is 0, we don't care how long ago the update came in, we're
+   * still going to use the gps] If time factor is high, we will prefer odometry
+   * if the gps update came in a while ago The actual value of this is
+   * unbeknownst to me.
+   */
+  double time_factor;
 
 } odom_gps_cfg_t;
 
-class OdometryGPS : OdometryBase
-{
-    public:
+class OdometryGPS : OdometryBase {
+public:
+  OdometryGPS(odom_gps_cfg_t &cfg, OdometryTank &enc_odom, gps &gps_sensor,
+              bool is_async = true);
+  OdometryGPS(odom_gps_cfg_t &cfg, Odometry3Wheel &enc_odom, gps &gps_sensor,
+              bool is_async = true);
 
-    OdometryGPS(odom_gps_cfg_t &cfg, OdometryTank &enc_odom, gps &gps_sensor, bool is_async=true);
-    OdometryGPS(odom_gps_cfg_t &cfg, Odometry3Wheel &enc_odom, gps &gps_sensor, bool is_async=true);
+  /**
+   * Update the current position on the field based on the sensors
+   * @return the location that the robot is at after the odometry does its
+   * calculations
+   */
+  pose_t update() override;
 
-    /**
-     * Update the current position on the field based on the sensors
-     * @return the location that the robot is at after the odometry does its calculations
-     */
-    pose_t update() override;
+private:
+  /**
+   * @return alpha: 0 when we should favor GPS position. 1 when we should favor
+   * encoder based odom. 0 when we're far away from the wall we're looking at
+   * and gps is worse. 1 when we're up close to the wall we're looking at and
+   * gps is better
+   */
+  double get_alpha();
 
-    private:
-
-    double get_alpha();
-
-    odom_gps_cfg_t &cfg;
-    OdometryBase &enc_odom;
-    gps &gps_sensor;
-    timer tmr;
+  odom_gps_cfg_t &cfg;
+  OdometryBase &enc_odom;
+  gps &gps_sensor;
+  timer tmr;
 };
--- core/src/subsystems/odometry/odometry_gps.cpp
+++ core/src/subsystems/odometry/odometry_gps.cpp
@@ -1,85 +1,84 @@
 #include "../core/include/subsystems/odometry/odometry_gps.h"
 
-OdometryGPS::OdometryGPS(odom_gps_cfg_t &cfg, OdometryTank &enc_odom, gps &gps_sensor, bool is_async) : 
-cfg(cfg), enc_odom(enc_odom), OdometryBase(is_async), gps_sensor(gps_sensor)
-{
+OdometryGPS::OdometryGPS(odom_gps_cfg_t &cfg, OdometryTank &enc_odom,
+                         gps &gps_sensor, bool is_async)
+    : cfg(cfg), enc_odom(enc_odom), OdometryBase(is_async),
+      gps_sensor(gps_sensor) {}
 
+OdometryGPS::OdometryGPS(odom_gps_cfg_t &cfg, Odometry3Wheel &enc_odom,
+                         gps &gps_sensor, bool is_async)
+    : cfg(cfg), enc_odom(enc_odom), OdometryBase(is_async),
+      gps_sensor(gps_sensor) {}
+
+double OdometryGPS::get_alpha() {
+  pose_t robot_pose = get_position();
+
+  // Part 1 - relative angle to the center of the field (as a scalar)
+  Vector2D position_vec({.x = 72 - robot_pose.x, .y = 72 - robot_pose.y});
+  position_vec = position_vec.normalize();
+  Vector2D rotation_vec(deg2rad(robot_pose.rot), 1);
+  double dot_prod = position_vec.dot(rotation_vec);
+
+  // Part 2 - Distance to center of field
+  double dist_scalar = robot_pose.get_point().dist({72, 72}) / 101.8;
+  dist_scalar = clamp(dist_scalar, 0.0, 1.0);
+
+  // Part 3 adjust distance by angle such that it becomes normalized distance to
+  // wall the sensor is looking at 0 when we're very far away and gps is better,
+  // 1 when we're very close to a wall and gps isn't great
+  double alpha = (pow(dist_scalar * dot_prod, cfg.distance_power) + 1) / 2.0;
+
+  return alpha;
 }
 
-OdometryGPS::OdometryGPS(odom_gps_cfg_t &cfg, Odometry3Wheel &enc_odom, gps &gps_sensor, bool is_async) :
-cfg(cfg), enc_odom(enc_odom), OdometryBase(is_async), gps_sensor(gps_sensor)
-{
+pose_t OdometryGPS::update() {
+  pose_t enc_pose = OdometryBase::update();
+  pose_t gps_pose = {.x = gps_sensor.xPosition(distanceUnits::in) + 72,
+                     .y = gps_sensor.yPosition(distanceUnits::in) + 72,
+                     .rot = gps_sensor.heading(rotationUnits::deg)};
 
-}
+  // Multiply by the time delta to adjust for runtime inconsistencies
+  // (Running faster / slower changes )
+  // faster update means we haven't moved that far since when we read the
+  // position
+  //
 
-double OdometryGPS::get_alpha()
-{
-    pose_t robot_pose = get_position();
-
-    // Part 1 - relative angle to the center of the field (as a scalar)
-    Vector2D position_vec({.x=72-robot_pose.x, .y=72-robot_pose.y});
-    position_vec = position_vec.normalize();
-    Vector2D rotation_vec(deg2rad(robot_pose.rot), 1);
-    double dot_prod = position_vec.dot(rotation_vec);
-
-    // Part 2 - Distance to center of field
-    double dist_scalar = robot_pose.get_point().dist({72, 72}) / 101.8; // (0 to 1)
-    
-    double alpha = ((dist_scalar * dot_prod) + 1) / 2.0;
-
-    return alpha;
-}
-
-pose_t OdometryGPS::update()
-{
-    pose_t enc_pose = OdometryBase::update();
-    pose_t gps_pose = {
-        .x = gps_sensor.xPosition(distanceUnits::in) + 72,
-        .y = gps_sensor.yPosition(distanceUnits::in) + 72,
-        .rot = gps_sensor.heading(rotationUnits::deg)
-    };
-    
-    // Multiply by the time delta to adjust for runtime inconsistencies
-    // (Running faster / slower changes )
-
-    if(fabs(enc_pose.get_point().dist(gps_pose.get_point())) > cfg.cutoff_radius)
-    {
-        tmr.reset();
-        return enc_pose;
-    }
-
-    double alpha_adjusted = get_alpha() * tmr.time(sec);
+  if (fabs(enc_pose.get_point().dist(gps_pose.get_point())) >
+      cfg.cutoff_radius) {
     tmr.reset();
+    return enc_pose;
+  }
 
-    double x = (alpha_adjusted * gps_pose.x) + ((1-alpha_adjusted) * enc_pose.x);
-    double y = (alpha_adjusted * gps_pose.y) + ((1-alpha_adjusted) * enc_pose.y);
-    double rot;
+  double alpha_adjusted = get_alpha() * tmr.time(sec) * cfg.time_factor;
 
-    // Handle the issue of averageing when crossing the 360/0 threshold
-    if ( fabs(gps_pose.rot - enc_pose.rot) > 180 )
-    {
-        if (gps_pose.rot < enc_pose.rot)
-            gps_pose.rot += 360;
-        else
-            enc_pose.rot += 360;
-    }
+  tmr.reset();
 
-    rot = (alpha_adjusted * gps_pose.rot) + ((1-alpha_adjusted) * enc_pose.rot);
+  double x =
+      (alpha_adjusted * gps_pose.x) + ((1 - alpha_adjusted) * enc_pose.x);
+  double y =
+      (alpha_adjusted * gps_pose.y) + ((1 - alpha_adjusted) * enc_pose.y);
+  double rot;
 
-    // Wrap the heading between 0 and 360
-    if (rot > 360)
-        rot -= 360;
-    else if (rot < 0)
-        rot += 360;
+  // Handle the issue of averageing when crossing the 360/0 threshold
+  if (fabs(gps_pose.rot - enc_pose.rot) > 180) {
+    if (gps_pose.rot < enc_pose.rot)
+      gps_pose.rot += 360;
+    else
+      enc_pose.rot += 360;
+  }
 
-    pose_t out = {
-        .x = x,
-        .y = y,
-        .rot = rot
-    };
+  rot = (alpha_adjusted * gps_pose.rot) + ((1 - alpha_adjusted) * enc_pose.rot);
 
-    enc_odom.set_position(out);
-    set_position(out);
+  // Wrap the heading between 0 and 360
+  if (rot > 360)
+    rot -= 360;
+  else if (rot < 0)
+    rot += 360;
 
-    return out;
+  pose_t out = {.x = x, .y = y, .rot = rot};
+
+  enc_odom.set_position(out);
+  set_position(out);
+
+  return out;
 }
