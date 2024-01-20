c7ca330d5ccfdae2dc7e675025a8531edb122913
cowsed <richie.sommers@outlook.com>
2024-01-19 23:56:00 -0500

    tested core as static lib, added more warnings

---
+++ core/build/CMakeFiles/Core.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/custom_encoder.cpp" "CMakeFiles/Core.dir/src/subsystems/custom_encoder.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/custom_encoder.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/flywheel.cpp" "CMakeFiles/Core.dir/src/subsystems/flywheel.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/flywheel.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/fun/video.cpp" "CMakeFiles/Core.dir/src/subsystems/fun/video.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/fun/video.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/mecanum_drive.cpp" "CMakeFiles/Core.dir/src/subsystems/mecanum_drive.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/mecanum_drive.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/odometry/odometry_3wheel.cpp" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_3wheel.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_3wheel.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/odometry/odometry_base.cpp" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_base.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_base.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/odometry/odometry_tank.cpp" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_tank.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/odometry/odometry_tank.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/screen.cpp" "CMakeFiles/Core.dir/src/subsystems/screen.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/screen.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/subsystems/tank_drive.cpp" "CMakeFiles/Core.dir/src/subsystems/tank_drive.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/subsystems/tank_drive.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/auto_chooser.cpp" "CMakeFiles/Core.dir/src/utils/auto_chooser.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/auto_chooser.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/command_structure/auto_command.cpp" "CMakeFiles/Core.dir/src/utils/command_structure/auto_command.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/command_structure/auto_command.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/command_structure/basic_command.cpp" "CMakeFiles/Core.dir/src/utils/command_structure/basic_command.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/command_structure/basic_command.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/command_structure/command_controller.cpp" "CMakeFiles/Core.dir/src/utils/command_structure/command_controller.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/command_structure/command_controller.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/command_structure/drive_commands.cpp" "CMakeFiles/Core.dir/src/utils/command_structure/drive_commands.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/command_structure/drive_commands.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/command_structure/flywheel_commands.cpp" "CMakeFiles/Core.dir/src/utils/command_structure/flywheel_commands.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/command_structure/flywheel_commands.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/bang_bang.cpp" "CMakeFiles/Core.dir/src/utils/controls/bang_bang.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/bang_bang.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/feedforward.cpp" "CMakeFiles/Core.dir/src/utils/controls/feedforward.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/feedforward.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/motion_controller.cpp" "CMakeFiles/Core.dir/src/utils/controls/motion_controller.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/motion_controller.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/pid.cpp" "CMakeFiles/Core.dir/src/utils/controls/pid.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/pid.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/pidff.cpp" "CMakeFiles/Core.dir/src/utils/controls/pidff.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/pidff.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/controls/take_back_half.cpp" "CMakeFiles/Core.dir/src/utils/controls/take_back_half.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/controls/take_back_half.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/generic_auto.cpp" "CMakeFiles/Core.dir/src/utils/generic_auto.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/generic_auto.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/graph_drawer.cpp" "CMakeFiles/Core.dir/src/utils/graph_drawer.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/graph_drawer.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/logger.cpp" "CMakeFiles/Core.dir/src/utils/logger.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/logger.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/math_util.cpp" "CMakeFiles/Core.dir/src/utils/math_util.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/math_util.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/moving_average.cpp" "CMakeFiles/Core.dir/src/utils/moving_average.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/moving_average.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/pure_pursuit.cpp" "CMakeFiles/Core.dir/src/utils/pure_pursuit.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/pure_pursuit.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/serializer.cpp" "CMakeFiles/Core.dir/src/utils/serializer.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/serializer.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/trapezoid_profile.cpp" "CMakeFiles/Core.dir/src/utils/trapezoid_profile.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/trapezoid_profile.cpp.obj.d"
+  "/home/richie/Clubs/VEX/JunderCMAKE/core/src/utils/vector2d.cpp" "CMakeFiles/Core.dir/src/utils/vector2d.cpp.obj" "gcc" "CMakeFiles/Core.dir/src/utils/vector2d.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
